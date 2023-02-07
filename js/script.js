e86630abb70c3fe862fcb4d9a153bdde6f5982f5
Marajul-BS23 <m.marajul+github@gmail.com>
2023-02-07 19:18:58 +0600

    [Added]: JavaScript file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// close the modal if close button is pressed
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+// open the modal if the button is pressed
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+}
+
+function openWarningModal() {
+    document.getElementById('warning_modal').style.display = 'block';
+}
+function closeWarningModal() {
+    document.getElementById('warning_modal').style.display = 'none';
+}
+
